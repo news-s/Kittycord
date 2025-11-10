837ea44582c48929894f7f91809bb4d7755f2d3a
MiloszSzczechowski <144437079+MiloszSzczechowski@users.noreply.github.com>
2025-11-10 22:39:51 +0100

    branch init

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,11 @@
-print("Hello World!")
-print("Test")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/ping")
+async def pong():
+    return {"ping": "pong"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
